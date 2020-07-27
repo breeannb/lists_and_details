35266d8f051ea394a26427132182fe28acb4f110
Breeann Bolinsky <bolinskybm10@gmail.com>
2020-07-27 13:39:06 -0700

    added the file structure and beginnings of react router

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,22 @@
 import React from 'react';
+import { 
+  BrowserRouter as Router, 
+  Switch, 
+  Route
+} from 'react-router-dom'; 
+
+import HomeList from './homelist/HomeList';
+import DetailPage from './detailpage/DetailPage';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <Router>
+      <h1>My Color App on all pages</h1>
+      <Switch>
+        <Route exact path="/:id" component={DetailPage} />
+        <Route exact path="/" component={HomeList} />
+      </Switch>
+    </Router>
+  );
 }
   
