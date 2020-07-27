545e4b41e5760c87d299b3af9eba083ca8997321
Breeann Bolinsky <bolinskybm10@gmail.com>
2020-07-27 16:48:57 -0700

    created api call for one character by id

--- src/components/App/services/getAvatarCharactersList.js
+++ src/components/App/services/getAvatarCharactersList.js
@@ -7,3 +7,18 @@ export const getAvatarCharactersList = () => {
       photoUrl: character.photoUrl
     })));
 };
+
+export const getOneAvatarCharacter = () => {
+  return fetch(`https://last-airbender-api.herokuapp.com//api/v1/characters/${id}`)
+    .then(res => res.json())
+    .then(characters => characters.map(character => ({
+      allies: character.allies, 
+      enemies: character.enemies, 
+      photoUrl: character.photoUrl,
+      name: character.name, 
+      _id: character._id,
+      gender: character.gender,
+      first: character.first, 
+      affilition: character.affilition
+    })));
+};
